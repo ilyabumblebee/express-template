4d96693e847614deb82cf9cf31e38e36c2743cb8
ilyabumblebee <hipomotgames@gmail.com>
2024-03-13 02:57:00 +0200

    semicolon

--- routes/healthz.js
+++ routes/healthz.js
@@ -5,6 +5,6 @@ const router = express.Router()
 /* GET healthz */
 router.get('/', async (req, res) => {
   res.status(200).json({ status: "success", message: "Service is up and running." })
-});
+})
 
 module.exports = router
