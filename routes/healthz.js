b74bf0b35d5d23dee46b0d424425efd5e1b203c5
bumblebee <hipomotgames@gmail.com>
2024-03-28 13:17:18 +0100

    Update healthz.js

--- routes/healthz.js
+++ routes/healthz.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET healthz */
 router.get('/', async (req, res) => {
-  res.status(200).json({ status: "success", message: "Service is up and running." })
+  res.status(200).json({ status: "success", message: "service is up and running." })
 })
 
 module.exports = router
